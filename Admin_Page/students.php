8e64b4f9b6e6e6a55be79f61b3cf255b39b0151c
Kurusaki21 <kurusakiibara@gmail.com>
2023-12-27 17:09:32 +0800

    /

--- Admin_Page/students.php
+++ Admin_Page/students.php
@@ -21,6 +21,7 @@ if(isset($user)){
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'includes/header.php'; ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 <style>
     .dashboard-image1{
       position: absolute;
@@ -376,11 +377,18 @@ if(isset($user)){
                             </div>
 
                             <div class="form-row col-md-12">
-                                    <input type="file" name="item_photo" value=""  onchange="loadFile(event)">
+                                    <input type="file" name="item_photo" value=""  onchange="loadFile(event)">   <input type=button value="Take Snapshot" onClick="take_snapshot()">
+                                    <div class="col-md-6">
+                                    
+                                        <br/>
+                                  
+                                        <input type="hidden" name="image" class="image-tag">
+                                    </div>
                                  
                             </div>
+                            <div class="form-row col-md-12">   <div id="my_camera"></div></div>
                             <div class="form-row col-md-12 ">
-                                <img id="preview" src="#" />
+                                <img id="preview" src="#" /> 
                             </div>
 
                         </div>
@@ -568,6 +576,25 @@ if(isset($user)){
         }
 
     </script>
+    <script language="JavaScript">
+    Webcam.set({
+        width: 490,
+        height: 390,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+  
+    Webcam.attach( '#my_camera' );
+  
+    function take_snapshot(e) {
+        Webcam.snap( function(data_uri) {
+           var img = $(".image-tag").val(data_uri);
+            document.getElementById('preview').append('<img src="'+data_uri+'"/>');
+            console.log(data_uri)
+        } );
+ 
+    }
+</script>
 </body>
 
 </html>
